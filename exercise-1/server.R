e4cd8f628a2ba7c4484a49b46af87c53e41c1745
Student User <studentuser@D-10-19-117-46.dhcp4.washington.edu>
2018-05-17 12:16:19 -0700

    Finish participation 8

--- exercise-1/server.R
+++ exercise-1/server.R
@@ -2,16 +2,18 @@
 library(dplyr)
 
 # Read in data
-source('./scripts/build_map.R')
 source('./scripts/build_scatter.R')
-df <- read.csv('./data/electoral_college.csv', stringsAsFactors = FALSE)
-state_codes <- read.csv('./data/state_codes.csv', stringsAsFactors = FALSE)
+source('./scripts/build_map.R')
+
+df <- read.csv("./data/electoral_college.csv", stringsAsFactors = FALSE)
+state_codes <- read.csv("./data/state_codes.csv", stringsAsFactors = FALSE)
 
 # Join together state.codes and df
-joined_data <- left_join(df, state_codes, by="state")
+joined_data <- left_join(df, state_codes, by = "state")
 
 # Compute the electoral votes per 100K people in each state
-joined_data <- joined_data %>% mutate(ratio = votes/population * 100000)
+joined_data <- joined_data %>% 
+  mutate(ratio = votes/population * 100000)
 
 # Start shinyServer
 shinyServer(function(input, output) { 
