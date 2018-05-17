ff055e5562cfef7863e820732778ad16c51345c6
Student User <studentuser@D-10-19-117-46.dhcp4.washington.edu>
2018-05-17 12:13:38 -0700

    Finish participation 8

--- exercise-1/ui.R
+++ exercise-1/ui.R
@@ -1,36 +1,45 @@
 # ui.R
 library(shiny)
 library(plotly)
-shinyUI(navbarPage(
-  "Electoral College",
-  # Create a tab panel for your map
-  tabPanel(
-    "Map",
-    titlePanel("Electoral College Votes"),
-    # Create sidebar layout
-    sidebarLayout(
-
-      # Side panel for controls
-      sidebarPanel(
-
-        # Input to select variable to map
-        selectInput(
-          "mapvar",
-          label = "Variable to Map",
-          choices = list(
-            "Population" = "population",
-            "Electoral Votes" = "votes",
-            "Votes / Population" = "ratio"
-          )
-        )
-      ),
-
-      # Main panel: display plotly map
-      mainPanel(
-        plotlyOutput("map")
-      )
-    )
-  )
-
-  # Create a tabPanel to show your scatter plot
-))
+library(shinythemes)
+shinyUI(navbarPage(theme = shinytheme("cerulean"),
+                   "Electoral College",
+                   # Create a tab panel for your map
+                   tabPanel(
+                     "Map",
+                     titlePanel("Votas del Colegio Electoral"),
+                     # Create sidebar layout
+                     sidebarLayout(
+                       
+                       # Side panel for controls
+                       sidebarPanel(
+                         tags$h2("Esa es una mapa interactiva"),
+                         # Input to select variable to map
+                         selectInput(
+                           "mapvar",
+                           label = "Variable to Map",
+                           choices = list(
+                             "Population" = "population",
+                             "Number of Electoral Votes" = "votes",
+                             "Votes per Person" = "ratio"
+                           )
+                         ), 
+                         tags$br(),
+                         HTML("<strong>Soy... fatigada</strong>"),
+                         tags$div(class = "sidebar"),
+                         tags$blockquote("Soy un blockquote flotante!")
+                       ),
+                       
+                       # Main panel: display plotly map
+                       mainPanel(
+                         plotlyOutput("map"),
+                         tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+                         tags$div(class = "summary", checked = NA,
+                                  tags$p("Esa mapa muestra el colegio electoral por estados. El mas oscuro
+                                         el color, el mas alta el numero de votas."))
+                                  )
+                       )
+                     )
+                   
+                   # Create a tabPanel to show your scatter plot
+                   ))
